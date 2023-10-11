8168176e7e2afad7d973abb917ce236accb6e5ae
vivek <122772279+viveksingh2211@users.noreply.github.com>
2023-10-11 05:40:16 +0000

    var_and_dataType

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 29843 // We can't change the variable if you are initlizing first time.
+var accountName = "vivek"
+let accountPassword = "20i4j"
+accountEmail = '@ybl.com'
+
+accountBalance = 399
+
+accountName = "singh"
+
+
+console.log(accountId)
+console.log(accountName)
+console.log(accountPassword)
+console.log(accountEmail)
+console.log(accountBalance)
+
+console.table(['accountId','accountName','accountPassword'])
---
+++ 01_Basics/02_data_Type.js
@@ -0,0 +1,12 @@
+let name = "Vivek"
+let age = 22
+let logIn = true
+let first = null
+
+// number => 2 to power 53
+// bigint 
+// string =>""
+// boolean => true/false
+// null => standalone value  --> null is also the "object" Datatype.
+// undefined =>
+// symbol => unique
